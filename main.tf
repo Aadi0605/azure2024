cf3ac146aeb21cbda9cc08d4640d0a40a4d3f3bc
aadityadwivedi32@gmail.com <aadityadwivedi32@gmail.com>
2024-05-03 00:56:29 +0530

    add2

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 provider "azurerm" {
 
-version ="`>v1.8.2"
+version ="~>v1.8.2"
 features{}
 
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "West Europe"
 }
